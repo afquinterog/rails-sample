c4535947715c8ad8c2a574604d44752bb787cde4
Andres Quintero <aquintero@oru.com.co>
2019-02-21 11:57:15 -0500

    Include docker-compose

--- app/jobs/sample_job_job.rb
+++ app/jobs/sample_job_job.rb
@@ -4,5 +4,9 @@ class SampleJobJob < ApplicationJob
   def perform(*args)
     # Do something later
     logger.debug "Sample job args: #{ args }"
+    
+    #@post = Post.create(title: "From job", body: "Sample from job")
+    
+    #user = User.create(name: "David", occupation: "Code Artist")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_02_04_235505) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
